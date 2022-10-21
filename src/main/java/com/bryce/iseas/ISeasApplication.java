40f4631dc2f520acbee3ac732717e78242fb181f
bryce <august17360@gmail.com>
2022-10-21 18:25:13 +0800

    v00.00.00 Springboot initialized.

---
+++ src/main/java/com/bryce/iseas/ISeasApplication.java
@@ -0,0 +1,13 @@
+package com.bryce.iseas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISeasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ISeasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryce/iseas/ISeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryce.iseas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISeasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
